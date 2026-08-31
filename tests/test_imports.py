e5324e80db33a58319051fc5c4fbabe5378ec9d2
Ahmed Yasser <138807222+Ahmed-yasser86@users.noreply.github.com>
2026-08-31 19:47:42 +0300

    Delete tests directory

--- tests/test_imports.py
+++
@@ -1,6 +0,0 @@
-import importlib
-
-
-def test_ingestion_embeddings_imports_cleanly():
-    module = importlib.import_module("Ingestion_Pipline.infra.embeddings")
-    assert hasattr(module, "build_embeddings")
